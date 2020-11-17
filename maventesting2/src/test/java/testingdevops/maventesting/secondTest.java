a4a92366ca8f3846e88be5b9283895ccda4753a1
CP002 <CP002@DESKTOP-USIB291>
2020-11-17 16:38:59 +0530

    browser scenario added

--- maventesting2/src/test/java/testingdevops/maventesting/secondTest.java
+++ maventesting2/src/test/java/testingdevops/maventesting/secondTest.java
@@ -1,5 +1,7 @@
 package testingdevops.maventesting;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class secondTest {
@@ -8,6 +10,10 @@ public class secondTest {
 	public void sampletest()
 	{
 		System.out.println("am second working");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\CP002\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		
 		
 	}
 
