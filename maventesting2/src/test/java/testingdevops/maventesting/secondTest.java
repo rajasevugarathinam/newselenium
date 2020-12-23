8619d2cd78f7ce1540bde3a0719afaf4baa2170b
CP002 <raja.sevugarathinam@convergepoint.com>
2020-12-23 12:14:26 +0530

    updated one

--- maventesting2/src/test/java/testingdevops/maventesting/secondTest.java
+++ maventesting2/src/test/java/testingdevops/maventesting/secondTest.java
@@ -10,9 +10,10 @@ public class secondTest {
 	public void sampletest() throws InterruptedException
 	{
 		System.out.println("am second working");
-		/*String chrome_driver_path= "/maventesting2/driver/chromedriver.exe";*/
-		
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "//driver/chromedriver.exe" );
+		/*String chrome_driver_path= "/maventesting2/driver/chromedriver.exe";*/		
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\driver\\chromedriver.exe" );
+		String chrome_path =  System.getProperty("user.dir") + ("\\driver\\chromedriver.exe");
+		System.out.println(chrome_path);
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
 		
