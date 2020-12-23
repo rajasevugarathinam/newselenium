56d6d62b63d446e1093c65bcc6743e746595c2fb
unknown <raja.sevugarathinam@convergepoint.com>
2020-12-23 13:56:06 +0530

    updated

--- maventesting2/src/test/java/testingdevops/maventesting/secondTest.java
+++ maventesting2/src/test/java/testingdevops/maventesting/secondTest.java
@@ -11,10 +11,10 @@ public class secondTest {
 	{
 		System.out.println("am second working");
 		/*String chrome_driver_path= "/maventesting2/driver/chromedriver.exe";*/		
-		/*System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\driver\\chromedriver.exe" );
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\driver\\chromedriver.exe" );
 		String chrome_path =  System.getProperty("user.dir") + ("\\driver\\chromedriver.exe");
-		System.out.println(chrome_path);*/
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\CP002\\git\\newselenium\\maventesting2\\driver\\chromedriver.exe");
+		System.out.println(chrome_path);
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\CP002\\git\\newselenium\\maventesting2\\driver\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
